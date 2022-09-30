a1d676ab596c7ec1deace0bd7856f558830cecc5
AADILA JASMIN <67281453+aadi1405@users.noreply.github.com>
2022-09-30 18:46:52 +0000

    the main function

--- function.py
+++ function.py
@@ -14,24 +14,46 @@ def loadData():
 
 @app.command()
 def viewdata():
-    """fetch items list"""
+    """fetch the data"""
     df = pd.read_csv('/workspaces/IDS706_Project2_AJ/dataset/Student_Study_data.csv')
     print(df)
 
 
 @app.command()
 def liststudents():
-    """return sorted list by calories"""
+    """return the number of entries of each student"""
     df = pd.read_csv('/workspaces/IDS706_Project2_AJ/dataset/Student_Study_data.csv')
     count1 = df['Name'].value_counts()
     print(count1)
 
 @app.command()
-def seemax():
-    """See max value of each dataset"""
-    df = pd.read_csv('menu.csv')
-    max_values = df[['Calories','Total Fat','Carbohydrates','Dietary Fiber','Sugars','Protein','Vitamin A (% Daily Value)','Vitamin C (% Daily Value)','Calcium (% Daily Value)','Iron (% Daily Value)']].max()
-    print(max_values)
+def studyhour():
+    """return the total study hour of the week of each student"""
+    df = pd.read_csv('/workspaces/IDS706_Project2_AJ/dataset/Student_Study_data.csv')
+    studyhour = df.groupby('Name')['study_hour'].sum()
+    print("the weekly study time of each student is: ")
+    print(studyhour)
+
+@app.command()
+def sleephour():
+    """return the total sleep hours for the week of each student"""
+    df = pd.read_csv('/workspaces/IDS706_Project2_AJ/dataset/Student_Study_data.csv')
+    studyhour = df.groupby('Name')['your sleep hour?'].sum()
+    print("the weekly sleep time of each student is: ")
+    print(studyhour)
+    mean = df['your sleep hour?'].mean()
+    print('the average sleep time is ' + str(mean))
+
+
+@app.command()
+def sleephour():
+    """return the total sleep hours for the week of each student"""
+    df = pd.read_csv('/workspaces/IDS706_Project2_AJ/dataset/Student_Study_data.csv')
+    studyhour = df.groupby('Name')['your sleep hour?'].sum()
+    print("the weekly sleep time of each student is: ")
+    print(studyhour)
+    mean = df['your sleep hour?'].mean()
+    print('the average sleep time is ' + str(mean))
 
 if __name__ == "__main__":
     app()
